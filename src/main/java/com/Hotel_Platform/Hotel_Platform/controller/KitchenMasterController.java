9b9049d7fc5461e60be35645c83f24643b693715
nilesh93Dev <nileshkumarpatna93@gmail.com>
2026-01-18 22:16:14 +0530

    Added KitchenMaster entity and KitchenMasterDTO

--- src/main/java/com/Hotel_Platform/Hotel_Platform/controller/KitchenMasterController.java
+++ src/main/java/com/Hotel_Platform/Hotel_Platform/controller/KitchenMasterController.java
@@ -5,6 +5,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -28,6 +29,7 @@ public class KitchenMasterController {
 	@Autowired
 	private KitchenMasterService kitchenMasterSer;
 	
+	@PostMapping
 	public ResponseEntity<SuccessResponse<KitchenMasterDTO>> createKitchen(
 			@RequestBody KitchenMasterDTO kitchenDto,
 			@RequestParam Long tenantId) {
